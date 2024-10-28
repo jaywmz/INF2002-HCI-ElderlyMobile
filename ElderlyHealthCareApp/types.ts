8ccf0b569a1fd2ec7296de07854f7ae6c8909540
joweenyx <angjoween@gmail.com>
2024-10-28 14:52:01 +0800

    feat: Update Settings Page

--- ElderlyHealthCareApp/types.ts
+++ ElderlyHealthCareApp/types.ts
@@ -13,13 +13,14 @@ export type RootStackParamList = {
     Locations: undefined;
     Timeslots: undefined;
     CreateApptConfirmation: undefined;
+    Setting: undefined; 
 };
 
 export type RegisteredUser = {
     username: string;
     password: string;
 } | null;
-  
+
 export type AuthProps = {
     registeredUser: RegisteredUser | null;
     setRegisteredUser: (user: RegisteredUser | null) => void;
