70f6fd4b5b1f500980442b477691a37077eaa2f2
joweenyx <angjoween@gmail.com>
2024-10-29 01:00:18 +0800

    Update ViewEditApptScreen.tsx

--- ElderlyHealthCareApp/screens/mainscreens/ViewEditApptScreen.tsx
+++ ElderlyHealthCareApp/screens/mainscreens/ViewEditApptScreen.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { RootStackParamList, AuthProps } from '../types';
-import { globalStyles } from '../styles/Theme';
+import { RootStackParamList, AuthProps } from '../../types';
+import { globalStyles } from '../../styles/Theme';
 
 type ViewEditApptScreenNavigationProp = StackNavigationProp<RootStackParamList, 'View/Edit Appointment'>;
 
@@ -33,7 +33,6 @@ const ViewEditApptScreen = ({ navigation, setRegisteredUser }: Props) => {
       </View>
 
       <TouchableOpacity style={styles.card} onPress={handleNavigateToEdit}>
-        <Image source={require('../assets/edit.jpg')} style={styles.icon} />
         <Text style={styles.cardTitle}>View/Edit Appointment</Text>
         <Text style={styles.cardSubtitle}>UI Card-based</Text>
       </TouchableOpacity>
