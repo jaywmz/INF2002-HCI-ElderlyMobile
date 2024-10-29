8061863ddabe744a51ce148b09236a382559f7b0
joweenyx <angjoween@gmail.com>
2024-10-29 12:36:06 +0800

    feat: Update ReminderScreen.tsx with AI

--- ElderlyHealthCareApp/App.tsx
+++ ElderlyHealthCareApp/App.tsx
@@ -108,6 +108,7 @@ function MainTabs({ registeredUser, setRegisteredUser, isAiEnabled }: AuthProps 
               {...props}
               registeredUser={registeredUser}
               setRegisteredUser={setRegisteredUser}
+              isAiEnabled={isAiEnabled}
             />
           )}
         </Tab.Screen>
